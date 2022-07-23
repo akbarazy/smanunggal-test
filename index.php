afac1b6a7464a6b34be180f41d1baab5e9dbca45
Akbar <tes@gmail.com>
2022-07-23 16:55:57 +0700

    Add navbar in index

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <body>
     <!-- header section -->
 
-    <div class="header border-bottom fixed-top py-3" id="header">
+    <div class="header bg-white border-bottom fixed-top py-3" id="header">
         <div class="container text-break">
             <a href="index.php" class="title-header nav-link">SMAN 1 KLAPANUNGGAL</a>
         </div>
@@ -29,12 +29,71 @@
 
     <!-- navbar section -->
 
+    <nav class="navbar navbar-expand bg-white fixed-bottom p-0" id="navbar">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse justify-content-center" id="navbarsExample02">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link navbar-links pt-2 pb-1 px-3 text-center navbar-home" href="#">
+                        <i class="fa fa-home fa-size-1" aria-hidden="true"></i>
+                        <p class="nav-text mb-0">Home</p>
+                        <p class="nav-hover">Home</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navbar-links pt-2 pb-1 px-3 text-center text-break" href="#">
+                        <i class="fa fa-users fa-size-2" aria-hidden="true"></i>
+                        <p class="nav-text mb-0">Organization</p>
+                        <p class="nav-hover">Organization</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link navbar-links pt-2 pb-1 px-3 text-center">
+                        <i class="fa fa-picture-o fa-size-3" aria-hidden="true"></i>
+                        <p class="nav-text mb-0">Facility</p>
+                        <p class="nav-hover">Facility</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link navbar-links pt-2 pb-1 px-3 text-center">
+                        <i class="fa fa-newspaper-o fa-size-4" aria-hidden="true"></i>
+                        <p class="nav-text mb-0">News</p>
+                        <p class="nav-hover">News</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link navbar-links pt-2 pb-1 px-3 text-center navbar-me">
+                        <img src="images/icon.svg" alt="icon" class="icon">
+                        <p class="nav-text mb-0">Me</p>
+                        <p class="nav-hover">Me</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <!-- end navbar -->
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script>
+        const navbarLinks = document.querySelectorAll(".navbar-links");
+        console.log(navbarLinks);
+        navbarLinks.forEach(function(navbarLinks) {
+            console.log(navbarLinks);
+            navbarLinks.addEventListener("mouseover", function(event) {
+                const navHover = navbarLinks.lastElementChild;
+                navHover.style.opacity = "1";
+            });
+            navbarLinks.addEventListener("mouseout", function(event) {
+                const navHover = navbarLinks.lastElementChild;
+                navHover.style.opacity = "0";
+            });
+        });
+    </script>
 </body>
 
 </html>
